5b6f7fc1d4f717d8b74bae2f7a7fc692afc90cec
Kamil Shakirov <kamils80@gmail.com>
2021-06-10 16:42:53 +0600

    WIP

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 ##### Options which a user might set before building go here #####
 
 # Where to install idris2 binaries and libraries
-PREFIX ?= $(HOME)/.idris2-float
+PREFIX ?= $(HOME)/.idris2
 
 # For Windows targets. Set to 1 to support Windows 7.
 OLD_WIN ?= 0
