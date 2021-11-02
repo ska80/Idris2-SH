idris-lang/Idris2

[refactor] Use multiline strings for better readability

src/Idris/SetOptions.idr
@@ -229,16 +229,15 @@ opts x _ = pure $ if (x `elem` optionFlags)
 
 -- bash autocompletion script using the given function name
 completionScript : (fun : String) -> String
-completionScript fun =
-  let fun' = "_" ++ fun
-   in unlines [ fun' ++ "()"
-              , "{"
-              , "  ED=$([ -z $2 ] && echo \"--\" || echo $2)"
-              , "  COMPREPLY=($(idris2 --bash-completion $ED $3))"
-              , "}"
-              , ""
-              , "complete -F " ++ fun' ++ " -o dirnames idris2"
-              ]
+completionScript fun = let fun' = "_" ++ fun in #"""
+\#{ fun' }()
+{
+  ED=$([ -z $2 ] && echo "--" || echo $2)
+  COMPREPLY=($(idris2 --bash-completion $ED $3))
+}
+
+complete -F \#{ fun' } -o dirnames idris2
+"""#
 
 --------------------------------------------------------------------------------
 --          Processing Options

ska80/Idris2-SH

src/Idris/SetOptions.idr
@@ -248,16 +248,15 @@ opts x _ = pure $ if (x `elem` optionFlags)
 
 -- bash autocompletion script using the given function name
 completionScript : (fun : String) -> String
-completionScript fun =
-  let fun' = "_" ++ fun
-   in unlines [ fun' ++ "()"
-              , "{"
-              , "  ED=$([ -z $2 ] && echo \"--\" || echo $2)"
-              , "  COMPREPLY=($(idris2 --bash-completion $ED $3))"
-              , "}"
-              , ""
-              , "complete -F " ++ fun' ++ " -o default idris2"
-              ]
+completionScript fun = let fun' = "_" ++ fun in #"""
+\#{ fun' }()
+{
+  ED=$([ -z $2 ] && echo "--" || echo $2)
+  COMPREPLY=($(idris2 --bash-completion $ED $3))
+}
+
+complete -F \#{ fun' } -o default idris2
+"""#
 
 --------------------------------------------------------------------------------
 --          Processing Options